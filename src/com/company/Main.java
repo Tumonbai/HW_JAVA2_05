6d893dc4a95d06ece6e572f0038201b1a646114f
Tumonbai <ztima90@gmail.com>
2020-01-27 23:18:39 +0600

    UP_TO_DATE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,34 +4,14 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 
 public class Main {
-    public static final int PASSENGERS_COUNT = 100;
-    public static CountDownLatch LATCH;
-
-
     public static void main(String[] args) throws InterruptedException {
         Semaphore sem = new Semaphore(4);
-        for (int i = 1; i < 100 ; i++) {
-            Passenger passenger = new Passenger(sem,i);
-            passenger.start();
-            
+        CountDownLatch countDownLatch = new CountDownLatch(100);
+        for (int i = 1; i < 100; i++) {
+            new Passenger(sem, countDownLatch, i).start();
         }
-        for (int i = 1; i < PASSENGERS_COUNT; i++) {
-            LATCH = new CountDownLatch(PASSENGERS_COUNT + 1);
-            Bus bus = new Bus(i);
-            bus.start();
-            Thread.sleep(1000);
+        countDownLatch.await();
 
-            try {
-                while (LATCH.getCount() > 100) {
-                    Thread.sleep(1000);
-                }
-            }
-            catch (NullPointerException ex)
-            {
-
-            }
-            
-        }
-
+        System.out.println("Автобус уезжает");
     }
 }
--- src/com/company/Passenger.java
+++ src/com/company/Passenger.java
@@ -5,30 +5,34 @@ import java.util.concurrent.Semaphore;
 
 public class Passenger extends Thread {
     Semaphore sem;
+    CountDownLatch countDownLatch;
     int id;
 
-    public Passenger (Semaphore sem, int id) {
+    public Passenger(Semaphore sem, CountDownLatch countDownLatch, int id) {
+        this.countDownLatch = countDownLatch;
         this.sem = sem;
         this.id = id;
-
     }
-    @Override
-    public void run (){
 
+    @Override
+    public void run() {
         try {
             sem.acquire();
+
             System.out.println("Пассажир " + id + " " + "обслуживается на кассе и приобрел свой билет");
             sleep(1000);
+
             System.out.println("Пассажир " + id + " " + "выходит из кассы");
             sem.release();
+
             sleep(1000);
+//            System.out.println("Пассажир " + id + " садиться в автобус");
+//            sleep(1000);
+            countDownLatch.countDown();
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
         }
-        catch (InterruptedException ie)
-        {
-        }
-        catch (IllegalMonitorStateException ex)
-        {
-        }
- }
+        super.run();
+    }
 }
 
